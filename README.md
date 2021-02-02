ffb0f03a1bb5783ae5103420730ef87bd752c1db
Ajay Jakate <ajay.jakate@gmail.com>
2021-02-02 17:30:42 -0600

    add readme; productionize

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Decryptor
+
+This is a tool used to decrypt password protected xlsx files using the command line
+
+## Building
+
+Simply run `mvn package`.
+You will see the fully functional and portable jar: `./target/decryptor.jar`
+
+## Usage
+1. Move the jar to the same directory as your encrypted xlsx file
+2. Run the java jar with 3 arguments, input file, output file, and password.
+
+    Example:
+
+    `java -jar decryptor.jar encryptedFile.xlsx unlocked.xlsx My$ecretpassw0rd101`
+
+3. You now should be able to open the unlocked xlsx file without a password
--- src/main/java/com/landscapehub/decryptor/App.java
+++ src/main/java/com/landscapehub/decryptor/App.java
@@ -10,22 +10,23 @@ import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+// Reference:
+// https://www.codejava.net/coding/java-example-to-read-password-protected-excel-files-using-apache-poi
+//
 public class App 
 {
     public static void main( String[] args )
     {
-        String excelFilePath = "avail.xlsx";
-
-        // String inputFile = args[0];
-        // String outputFile = args[1];
-        // String password = args[2];
+        String inputFile = args[0];
+        String outputFile = args[1];
+        String password = args[2];
 
         try {
-            NPOIFSFileSystem fileSystem = new NPOIFSFileSystem(new File(excelFilePath));
+            NPOIFSFileSystem fileSystem = new NPOIFSFileSystem(new File(inputFile));
             EncryptionInfo info = new EncryptionInfo(fileSystem);
             Decryptor decryptor = Decryptor.getInstance(info);
              
-            if (!decryptor.verifyPassword("plants")) {
+            if (!decryptor.verifyPassword(password)) {
                 System.out.println("Unable to process: document is encrypted.");
                 return;
             }
@@ -34,7 +35,7 @@ public class App
              
             Workbook workbook = new XSSFWorkbook(dataStream);
 
-            try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
+            try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
                 workbook.write(outputStream);
             }
              
